54073fe4c6c32db2436ceccca6decc1c55737e71
aantem <113061718+aantem@users.noreply.github.com>
2022-09-08 17:30:21 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 def majuscule(mot):
-    mot=list(mot)
-    for i in range(0,len(mot)):
-        mot[i]= chr(ord(mot[i]) - 32)
-    mot=''.join(mot)
     return mot
 
 
